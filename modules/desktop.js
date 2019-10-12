d3ab30855ffc76f39aec0773f2cec141d9053ffd
DanSoup <dansoup@hotmail.co.uk>
2019-10-12 11:30:30 +0100

    added write util

--- modules/desktop.js
+++ modules/desktop.js
@@ -1,14 +1,21 @@
+import utils from '../utils.js';
+
+const {write} = utils;
+
 const desktop = () => {
+
   for (let x = 0; x < gContent.width; x++) {
     gContent.text[0][x] = '═';
     gContent.text[2][x] = '═';
     gContent.text[gContent.height - 3][x] = '═';
     gContent.text[gContent.height - 1][x] = '═';
   };
+
   for (let y = 0; y < gContent.height; y++) {
     gContent.text[y][0] = '║';      
     gContent.text[y][gContent.width - 1] = '║';      
   };
+
   gContent.text[0][0] = '╔';
   gContent.text[2][0] = '╠';
   gContent.text[gContent.height - 3][0] = '╠';
@@ -18,6 +25,9 @@ const desktop = () => {
   gContent.text[2][gContent.width - 1] = '╣';
   gContent.text[gContent.height - 3][gContent.width - 1] = '╣';
   gContent.text[gContent.height - 1][gContent.width - 1] = '╝';
+
+  write([20, 20], 'Hello')
+
 };
 
 export default desktop;
--- utils.js
+++ utils.js
@@ -3,6 +3,27 @@ const utils = {
   randomChar: () => {
     const choice = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890';
     return choice[Math.floor(Math.random() * choice.length)]
+  },
+
+  write: (position, string) => {
+
+    if (!Array.isArray(position)) {
+      throw('"position" must be an array')
+    } else if (typeof position[0] !== 'number') {
+      throw('y coordinate must be a number')
+    } else if (typeof position[1] !== 'number') {
+      throw('x coordinate must be a number')      
+    } else if (typeof string !== 'string') {
+      throw('A string must be provided')
+    }
+
+    let [y, x] = position;
+
+    string.split('').forEach(char => {
+      if (x < gContent.width) gContent.text[y][x] = char;
+      x++
+    });
+
   }
   
 }
