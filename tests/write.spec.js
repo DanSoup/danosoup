1334bdd4f4c1601d217999b040beef0c7abce246
DanSoup <dansoup@hotmail.co.uk>
2019-10-14 21:38:19 +0100

    added jest in order to do testing

---
+++ tests/write.spec.js
@@ -0,0 +1,10 @@
+import utils from '../utils.js';
+import { isTSAnyKeyword } from '@babel/types';
+
+const {write} = utils;
+
+describe('test1', () => {
+  test('it is', () => {
+    expect(5).toEqual(5)
+  });
+});
